18291514b8625f091cf8de6d786cd43da2b104cf
ZLH <longhao.zlh@alibaba-inc.com>
2021-09-02 21:10:22 +0800

    fix estimate jacobian bug

--- demo.py
+++ demo.py
@@ -101,35 +101,6 @@ def get_rotation_matrix(yaw, pitch, roll):
 
     return rot_mat
 
-'''
-def get_rotation_matrix(yaw, pitch, roll):
-    yaw = yaw / 180 * 3.14
-    pitch = pitch / 180 * 3.14
-    roll = roll / 180 * 3.14
-
-    roll = roll.unsqueeze(1)
-    pitch = pitch.unsqueeze(1)
-    yaw = yaw.unsqueeze(1)
-
-    pitch_mat = torch.cat([torch.ones_like(pitch), torch.zeros_like(pitch), torch.zeros_like(pitch), 
-                          torch.zeros_like(pitch), torch.cos(pitch), -torch.sin(pitch),
-                          torch.zeros_like(pitch), torch.sin(pitch), torch.cos(pitch)], dim=1)
-    pitch_mat = pitch_mat.view(pitch_mat.shape[0], 3, 3)
-
-    yaw_mat = torch.cat([torch.cos(yaw), torch.zeros_like(yaw), torch.sin(yaw), 
-                           torch.zeros_like(yaw), torch.ones_like(yaw), torch.zeros_like(yaw),
-                           -torch.sin(yaw), torch.zeros_like(yaw), torch.cos(yaw)], dim=1)
-    yaw_mat = yaw_mat.view(yaw_mat.shape[0], 3, 3)
-
-    roll_mat = torch.cat([torch.cos(roll), -torch.sin(roll), torch.zeros_like(roll),  
-                         torch.sin(roll), torch.cos(roll), torch.zeros_like(roll),
-                         torch.zeros_like(roll), torch.zeros_like(roll), torch.ones_like(roll)], dim=1)
-    roll_mat = roll_mat.view(roll_mat.shape[0], 3, 3)
-
-    rot_mat = torch.einsum('bij,bjk,bkm->bim', pitch_mat, yaw_mat, roll_mat)
-
-    return rot_mat
-'''
 
 def keypoint_transformation(kp_canonical, he, estimate_jacobian=True, free_view=False, yaw=0, pitch=0, roll=0):
     kp = kp_canonical['value']
@@ -178,36 +149,6 @@ def keypoint_transformation(kp_canonical, he, estimate_jacobian=True, free_view=
 
     return {'value': kp_transformed, 'jacobian': jacobian_transformed}
 
-'''
-def make_animation(source_image, driving_video, generator, kp_detector, he_estimator, relative=True, adapt_movement_scale=True, cpu=False, free_view=False, yaw=0, pitch=0, roll=0):
-    with torch.no_grad():
-        predictions = []
-        source = torch.tensor(source_image[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
-        if not cpu:
-            source = source.cuda()
-        driving = torch.tensor(np.array(driving_video)[np.newaxis].astype(np.float32)).permute(0, 4, 1, 2, 3)
-        kp_canonical = kp_detector(source)
-        he_source = he_estimator(source)
-        he_driving_initial = he_estimator(driving[:, :, 0])
-
-        kp_source = keypoint_transformation(kp_canonical, he_source)
-        kp_driving_initial = keypoint_transformation(kp_canonical, he_driving_initial)
-        # kp_driving_initial = keypoint_transformation(kp_canonical, he_driving_initial, free_view=free_view, yaw=yaw, pitch=pitch, roll=roll)
-
-        for frame_idx in tqdm(range(driving.shape[2])):
-            driving_frame = driving[:, :, frame_idx]
-            if not cpu:
-                driving_frame = driving_frame.cuda()
-            he_driving = he_estimator(driving_frame)
-            kp_driving = keypoint_transformation(kp_canonical, he_driving, free_view=free_view, yaw=yaw, pitch=pitch, roll=roll)
-            kp_norm = normalize_kp(kp_source=kp_source, kp_driving=kp_driving,
-                                   kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
-                                   use_relative_jacobian=relative, adapt_movement_scale=adapt_movement_scale)
-            out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
-
-            predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
-    return predictions
-'''
 
 def make_animation(source_image, driving_video, generator, kp_detector, he_estimator, relative=True, adapt_movement_scale=True, estimate_jacobian=True, cpu=False, free_view=False, yaw=0, pitch=0, roll=0):
     with torch.no_grad():
