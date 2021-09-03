bef674d90850de541d3b9614e9b121dc8b06bab2
ZLH <longhao.zlh@alibaba-inc.com>
2021-09-03 10:43:51 +0800

    rotation matrix

--- demo.py
+++ demo.py
@@ -101,6 +101,35 @@ def get_rotation_matrix(yaw, pitch, roll):
 
     return rot_mat
 
+'''
+def get_rotation_matrix(yaw, pitch, roll):
+    yaw = yaw / 180 * 3.14
+    pitch = pitch / 180 * 3.14
+    roll = roll / 180 * 3.14
+
+    roll = roll.unsqueeze(1)
+    pitch = pitch.unsqueeze(1)
+    yaw = yaw.unsqueeze(1)
+
+    pitch_mat = torch.cat([torch.ones_like(pitch), torch.zeros_like(pitch), torch.zeros_like(pitch), 
+                          torch.zeros_like(pitch), torch.cos(pitch), -torch.sin(pitch),
+                          torch.zeros_like(pitch), torch.sin(pitch), torch.cos(pitch)], dim=1)
+    pitch_mat = pitch_mat.view(pitch_mat.shape[0], 3, 3)
+
+    yaw_mat = torch.cat([torch.cos(yaw), torch.zeros_like(yaw), torch.sin(yaw), 
+                           torch.zeros_like(yaw), torch.ones_like(yaw), torch.zeros_like(yaw),
+                           -torch.sin(yaw), torch.zeros_like(yaw), torch.cos(yaw)], dim=1)
+    yaw_mat = yaw_mat.view(yaw_mat.shape[0], 3, 3)
+
+    roll_mat = torch.cat([torch.cos(roll), -torch.sin(roll), torch.zeros_like(roll),  
+                         torch.sin(roll), torch.cos(roll), torch.zeros_like(roll),
+                         torch.zeros_like(roll), torch.zeros_like(roll), torch.ones_like(roll)], dim=1)
+    roll_mat = roll_mat.view(roll_mat.shape[0], 3, 3)
+
+    rot_mat = torch.einsum('bij,bjk,bkm->bim', pitch_mat, yaw_mat, roll_mat)
+
+    return rot_mat
+'''
 
 def keypoint_transformation(kp_canonical, he, estimate_jacobian=True, free_view=False, yaw=0, pitch=0, roll=0):
     kp = kp_canonical['value']
@@ -149,76 +178,6 @@ def keypoint_transformation(kp_canonical, he, estimate_jacobian=True, free_view=
 
     return {'value': kp_transformed, 'jacobian': jacobian_transformed}
 
-'''
-def keypoint_transformation(kp_canonical, he, estimate_jacobian=True, free_view=False, yaw=0, pitch=0, roll=0):
-    kp = kp_canonical['value']
-    if not free_view:
-        yaw, pitch, roll = he['yaw'], he['pitch'], he['roll']
-        yaw = headpose_pred_to_degree(yaw)
-        pitch = headpose_pred_to_degree(pitch)
-        roll = headpose_pred_to_degree(roll)
-        
-        t, exp = he['t'], he['exp']
-
-        rot_mat = get_rotation_matrix(yaw, pitch, roll)
-
-        # keypoint rotation
-        kp_rotated = torch.einsum('bmp,bkp->bkm', rot_mat, kp)
-
-        # keypoint translation
-        t = t.unsqueeze_(1).repeat(1, kp.shape[1], 1)
-        kp_t = kp_rotated + t
-
-        # add expression deviation 
-        exp = exp.view(exp.shape[0], -1, 3)
-        kp_transformed = kp_t + exp
-    else:
-        yaw_d, pitch_d, roll_d = he['yaw'], he['pitch'], he['roll']
-        yaw_d = headpose_pred_to_degree(yaw_d)
-        pitch_d = headpose_pred_to_degree(pitch_d)
-        roll_d = headpose_pred_to_degree(roll_d)
-        
-        if yaw is not None:
-            yaw = torch.tensor([yaw]).cuda()
-        else:
-            yaw = he['yaw']
-            yaw = headpose_pred_to_degree(yaw)
-        if pitch is not None:
-            pitch = torch.tensor([pitch]).cuda()
-        else:
-            pitch = he['pitch']
-            pitch = headpose_pred_to_degree(pitch)
-        if roll is not None:
-            roll = torch.tensor([roll]).cuda()
-        else:
-            roll = he['roll']
-            roll = headpose_pred_to_degree(roll)
-
-        t, exp = he['t'], he['exp']
-
-        rot_mat = get_rotation_matrix(yaw, pitch, roll)
-        rot_mat_d = get_rotation_matrix(yaw_d, pitch_d, roll_d)
-
-        # keypoint rotation
-        kp_rotated = torch.einsum('bmp,bkp->bkm', rot_mat, kp)
-
-        # keypoint translation
-        t = t.unsqueeze_(1).repeat(1, kp.shape[1], 1)
-        kp_t = kp_rotated + t
-
-        # add expression deviation 
-        exp = exp.view(exp.shape[0], -1, 3)
-        exp = torch.einsum('bij,bjp,bkp->bki', rot_mat, torch.inverse(rot_mat_d), exp)
-        kp_transformed = kp_t + exp
-
-    if estimate_jacobian:
-        jacobian = kp_canonical['jacobian']
-        jacobian_transformed = torch.einsum('bmp,bkps->bkms', rot_mat, jacobian)
-    else:
-        jacobian_transformed = None
-
-    return {'value': kp_transformed, 'jacobian': jacobian_transformed}
-'''
 
 def make_animation(source_image, driving_video, generator, kp_detector, he_estimator, relative=True, adapt_movement_scale=True, estimate_jacobian=True, cpu=False, free_view=False, yaw=0, pitch=0, roll=0):
     with torch.no_grad():
