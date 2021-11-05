d6a463d6a523944d20b97300233edb2aedde2cf3
ZLH <longhao.zlh@alibaba-inc.com>
2021-11-05 13:24:24 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -72,7 +72,8 @@ def headpose_pred_to_degree(pred):
 
     return degree
 
-
+'''
+# beta version
 def get_rotation_matrix(yaw, pitch, roll):
     yaw = yaw / 180 * 3.14
     pitch = pitch / 180 * 3.14
@@ -100,8 +101,8 @@ def get_rotation_matrix(yaw, pitch, roll):
     rot_mat = torch.einsum('bij,bjk,bkm->bim', roll_mat, pitch_mat, yaw_mat)
 
     return rot_mat
-
 '''
+
 def get_rotation_matrix(yaw, pitch, roll):
     yaw = yaw / 180 * 3.14
     pitch = pitch / 180 * 3.14
@@ -129,6 +130,55 @@ def get_rotation_matrix(yaw, pitch, roll):
     rot_mat = torch.einsum('bij,bjk,bkm->bim', pitch_mat, yaw_mat, roll_mat)
 
     return rot_mat
+
+'''
+# beta version
+def keypoint_transformation(kp_canonical, he, estimate_jacobian=True, free_view=False, yaw=0, pitch=0, roll=0):
+    kp = kp_canonical['value']
+    if not free_view:
+        yaw, pitch, roll = he['yaw'], he['pitch'], he['roll']
+        yaw = headpose_pred_to_degree(yaw)
+        pitch = headpose_pred_to_degree(pitch)
+        roll = headpose_pred_to_degree(roll)
+    else:
+        if yaw is not None:
+            yaw = torch.tensor([yaw]).cuda()
+        else:
+            yaw = he['yaw']
+            yaw = headpose_pred_to_degree(yaw)
+        if pitch is not None:
+            pitch = torch.tensor([pitch]).cuda()
+        else:
+            pitch = he['pitch']
+            pitch = headpose_pred_to_degree(pitch)
+        if roll is not None:
+            roll = torch.tensor([roll]).cuda()
+        else:
+            roll = he['roll']
+            roll = headpose_pred_to_degree(roll)
+
+    t, exp = he['t'], he['exp']
+
+    rot_mat = get_rotation_matrix(yaw, pitch, roll)
+    
+    # keypoint rotation
+    kp_rotated = torch.einsum('bmp,bkp->bkm', rot_mat, kp)
+
+    # keypoint translation
+    t = t.unsqueeze_(1).repeat(1, kp.shape[1], 1)
+    kp_t = kp_rotated + t
+
+    # add expression deviation 
+    exp = exp.view(exp.shape[0], -1, 3)
+    kp_transformed = kp_t + exp
+
+    if estimate_jacobian:
+        jacobian = kp_canonical['jacobian']
+        jacobian_transformed = torch.einsum('bmp,bkps->bkms', rot_mat, jacobian)
+    else:
+        jacobian_transformed = None
+
+    return {'value': kp_transformed, 'jacobian': jacobian_transformed}
 '''
 
 def keypoint_transformation(kp_canonical, he, estimate_jacobian=True, free_view=False, yaw=0, pitch=0, roll=0):
@@ -176,7 +226,7 @@ def keypoint_transformation(kp_canonical, he, estimate_jacobian=True, free_view=
     else:
         jacobian_transformed = None
 
-    return {'value': kp_transformed, 'jacobian': jacobian_transformed}
+    return {'value': kp_transformed, 'jacobian': jacobian_transformed, 'rot': rot_mat}
 
 
 def make_animation(source_image, driving_video, generator, kp_detector, he_estimator, relative=True, adapt_movement_scale=True, estimate_jacobian=True, cpu=False, free_view=False, yaw=0, pitch=0, roll=0):
@@ -202,7 +252,7 @@ def make_animation(source_image, driving_video, generator, kp_detector, he_estim
             kp_driving = keypoint_transformation(kp_canonical, he_driving, estimate_jacobian, free_view=free_view, yaw=yaw, pitch=pitch, roll=roll)
             kp_norm = normalize_kp(kp_source=kp_source, kp_driving=kp_driving,
                                    kp_driving_initial=kp_driving_initial, use_relative_movement=relative,
-                                   use_relative_jacobian=(estimate_jacobian&relative),
+                                   use_relative_jacobian=estimate_jacobian,
                                    adapt_movement_scale=adapt_movement_scale)
             out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
 
