56f7d199d2bffc49a1567881afa1922469eff7fb
ZLH <longhao.zlh@alibaba-inc.com>
2021-11-11 11:13:25 +0800

    Update animate.py

--- animate.py
+++ animate.py
@@ -11,8 +11,6 @@ import numpy as np
 
 from sync_batchnorm import DataParallelWithCallback
 
-'''
-# beta version
 def normalize_kp(kp_source, kp_driving, kp_driving_initial, adapt_movement_scale=False,
                  use_relative_movement=False, use_relative_jacobian=False):
     if adapt_movement_scale:
@@ -32,30 +30,5 @@ def normalize_kp(kp_source, kp_driving, kp_driving_initial, adapt_movement_scale
         if use_relative_jacobian:
             jacobian_diff = torch.matmul(kp_driving['jacobian'], torch.inverse(kp_driving_initial['jacobian']))
             kp_new['jacobian'] = torch.matmul(jacobian_diff, kp_source['jacobian'])
-
-    return kp_new
-    '''
-def normalize_kp(kp_source, kp_driving, kp_driving_initial, adapt_movement_scale=False,
-                 use_relative_movement=False, use_relative_jacobian=False):
-    if adapt_movement_scale:
-        source_area = ConvexHull(kp_source['value'][0].data.cpu().numpy()).volume
-        driving_area = ConvexHull(kp_driving_initial['value'][0].data.cpu().numpy()).volume
-        adapt_movement_scale = np.sqrt(source_area) / np.sqrt(driving_area)
-    else:
-        adapt_movement_scale = 1
-
-    kp_new = {k: v for k, v in kp_driving.items()}
-
-    if use_relative_movement:
-        kp_value_diff = (kp_driving['value'] - kp_driving_initial['value'])
-        kp_value_diff *= adapt_movement_scale
-        kp_new['value'] = kp_value_diff + kp_source['value']
-
-        if use_relative_jacobian:
-            jacobian_diff = torch.matmul(kp_driving['jacobian'], torch.inverse(kp_driving_initial['jacobian']))
-            kp_new['jacobian'] = torch.matmul(jacobian_diff, kp_source['jacobian'])
-        else:
-            rot_diff = torch.matmul(kp_driving['rot'], torch.inverse(kp_driving_initial['rot']))
-            kp_new['rot'] = torch.matmul(rot_diff, kp_source['rot'])
 
     return kp_new
